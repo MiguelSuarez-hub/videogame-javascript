d8b50898a30d451ba3bb2d3f65890e65057205bd
Mike <miguel.suarezf05@gmail.com>
2023-05-16 17:14:06 -0500

    added render of a map in the canvas

--- game.js
+++ game.js
@@ -1,10 +1,14 @@
 const canvas = document.querySelector('#game');
 const game = canvas.getContext('2d');
+let canvasSize;
+let elementSize;
+let map = maps[0].trim().split('\n');
+map = map.map((row) => row.trim().split(''));
 
-window.addEventListener('load', startGame);
+window.addEventListener('load', setCanvasSize);
+window.addEventListener('resize', setCanvasSize);
 
-function startGame() {
-  let canvasSize;
+function setCanvasSize() {
 
   if (window.innerHeight > window.innerWidth) {
     canvasSize = (window.innerWidth * 0.75)-16;
@@ -14,13 +18,18 @@ function startGame() {
 
   canvas.setAttribute('width', canvasSize);
   canvas.setAttribute('height', canvasSize);
-  const elementSize = (canvasSize / 10) - 1;
+  elementSize = (canvasSize / 10) - 1;
 
+  startGame();
+}
+
+function startGame() {
   game.font = elementSize + 'px Serif';
   game.textAlign = 'end';
 
-  for (let i = 1; i <= 10; i++) {
-    game.fillText(emojis['X'], (i * elementSize) + 14, elementSize);
-  }
-  
+  for (let row = 0; row < 10; row++) {   
+    for (let col = 0; col < 10; col++) {
+      game.fillText(emojis[map[row][col]], ((col + 1) * elementSize) + 14, (row + 1) * elementSize);
+    }    
+  } 
 }
