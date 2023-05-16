75eb6346595f484018a5dfbfa9b52fc6edaf246a
Mike <miguel.suarezf05@gmail.com>
2023-05-16 16:17:57 -0500

    added variable size to the canvas and his elements

--- game.js
+++ game.js
@@ -4,5 +4,23 @@ const game = canvas.getContext('2d');
 window.addEventListener('load', startGame);
 
 function startGame() {
-  game.fillRect(0,0,100,100);
+  let canvasSize;
+
+  if (window.innerHeight > window.innerWidth) {
+    canvasSize = (window.innerWidth * 0.75)-16;
+  } else {
+    canvasSize = (window.innerHeight * 0.75)-16;
+  }
+
+  canvas.setAttribute('width', canvasSize);
+  canvas.setAttribute('height', canvasSize);
+  const elementSize = (canvasSize / 10) - 1;
+
+  game.font = elementSize + 'px Serif';
+  game.textAlign = 'end';
+
+  for (let i = 1; i <= 10; i++) {
+    game.fillText(emojis['X'], (i * elementSize) + 14, elementSize);
+  }
+  
 }
